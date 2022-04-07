4dcc9dd24502eb98fab942b8af547592cf27bd10
Angelica Moberg <angelica.moberg@hotmail.com>
2022-04-07 16:43:07 +0200

    ÖVERFÖRING

--- wp-content/plugins/woocommerce/packages/woocommerce-blocks/build/mini-cart-frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wc-settings', 'wp-polyfill'), 'version' => 'c63a29c67698f9916cab4da7e5f054dc');
--- wp-content/plugins/woocommerce/packages/woocommerce-blocks/build/wc-blocks-shared-context.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-element', 'wp-polyfill'), 'version' => 'df23e14e237945e23701c6debefa414d');
