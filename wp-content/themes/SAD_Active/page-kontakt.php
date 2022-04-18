b966b13e7ada424e3ba23788c2add1ed759a8703
Angelica Moberg <angelica.moberg@hotmail.com>
2022-04-18 12:50:24 +0200

    Lade till butik- och kontaktsida

---
+++ wp-content/themes/SAD_Active/page-kontakt.php
@@ -0,0 +1,23 @@
+<!-- 
+    Den här sidan representerar just nu kontaktsidan.
+-->
+
+<?php echo "page-kontakt"; ?>
+
+<?php get_header(); ?>
+
+<main>
+   
+    <?php
+    while (have_posts()) {
+        the_post();  
+        
+        the_title();
+        the_content();  ?>
+        
+    <?php } ?>
+              
+</main>
+
+<?php get_footer(); ?>
+
