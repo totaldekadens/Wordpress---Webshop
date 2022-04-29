84604444d71b866acde1ed3b085415891dfaab52
SanazShahed <Shahed.sanaz12@gmail.com>
2022-04-29 11:19:36 +0200

    Butiker

--- wp-content/themes/SAD_Active/enqueue.php
+++ wp-content/themes/SAD_Active/enqueue.php
@@ -17,11 +17,18 @@ $uri = get_theme_file_uri();
  wp_register_style('frontPage', $uri . '/css/frontPage.css');
  wp_register_style('home', $uri . '/css/home.css');
  wp_register_style('single', $uri . '/css/single.css');
+ wp_register_style('bootstrap', $uri . '/css/bootstrap.min.css');
 
 // Registrerar script-filer.
+
+wp_register_script('popper.min', $uri . '/js/popper.min.js' );
+wp_register_script('bootstrap.min', $uri . '/js/bootstrap.min.js');
 wp_register_script('logic', $uri . '/js/logic.js', [], false, true );
 wp_register_script('productCategory', $uri . '/js/productCategory.js', [], false, true );
 wp_register_script('product', $uri . '/js/product.js', [], false, true );
+wp_register_script('product', $uri . '/js/product.js', [], false, true );
+wp_register_script('popper', $uri . '/js/popper.min.js' );
+wp_register_script('bootstrap', $uri . '/js/bootstrap.min.js',['popper'] );
 
 
 
@@ -31,6 +38,9 @@ wp_register_script('product', $uri . '/js/product.js', [], false, true );
 
 // css- script-filer  som läggs här hamnar i alla php-filer
  wp_enqueue_style('style');
+ wp_enqueue_style('bootstrap');
+ wp_enqueue_script('popper');
+ wp_enqueue_script('bootstrap');
 
 
 
